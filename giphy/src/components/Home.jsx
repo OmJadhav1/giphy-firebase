d2029b4a3a56565a5d174dd245a9ce5f283cddbe
omJadhav1 <omjadhav0462@gmail.com>
2024-11-07 23:05:42 +0530

    fetch gifs using giphy

--- giphy/src/components/Home.jsx
+++ giphy/src/components/Home.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import Signout from './signOut';
+import { useEffect, useState } from "react";
+import Signout from "./signOut";
 const Home = () => {
+    const [name, setName] = useState("");
+    const [gifs, setGifs] = useState([]);   
+  
+    useEffect(() => {
+      const fetchGifs = async () => {
+        const response = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=GlVGYHkr3WSBnllca54iNt0yFbjz7L65&q=${name}&limit=10`
+        );
+        const result = await response.json();
+        setGifs(result.data);
+      };
+  
+      fetchGifs();
+  
+    }, [name]);
   return (
     <>
-        <Signout />
-        Home
+      <Signout />
+      <input
+        type="text"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder="Search for GIFs"
+      />
+
+      <div>
+        {gifs.length > 0 ? (
+          gifs.map((gif) => (
+            <img
+              key={gif.id}
+              src={gif.images.fixed_height.url}
+              alt={gif.title}
+            />
+          ))
+        ) : (
+          <p>No results found</p>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
